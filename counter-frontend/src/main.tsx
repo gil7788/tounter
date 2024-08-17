09821b87af435ae8bd385e841df8ea458e003d81
gil7788 <gil7788@gmail.com>
2024-08-17 18:11:57 +0300

    Resolving "ton-core" and "@ton/core" packages conflict

--- counter-frontend/src/main.tsx
+++ counter-frontend/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const manifestUrl = "http://localhost:5173/tonconnect-manifest.json";
+
+createRoot(document.getElementById('root')! as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </StrictMode>,
+  </TonConnectUIProvider>
 )
